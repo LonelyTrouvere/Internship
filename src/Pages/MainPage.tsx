6a0f001b5fc212d00595da9246612ad2df5d2997
LonelyTrouvere <kobzar_stanislav@knu.ua>
2023-04-24 15:22:44 +0300

    Main page added

---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,12 @@
+
+const Main = () => {
+    return (
+        <div className="container" style={{textAlign:'center'}}>
+            <h1>Welcome</h1>
+            <h3>This is a Team Manager project</h3>
+            <h3>The goal is to help building a healthy and highly efficient crew</h3>
+        </div>
+    );
+}
+
+export default Main;
